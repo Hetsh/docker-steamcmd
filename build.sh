71730d437e841f3fa1038832707c80e4b62e2f66
Hetsh <git@hetsh.de>
2019-12-28 20:04:05 +0100

    Ask before testing image

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+trap "exit" SIGINT
+
+if ! docker version &> /dev/null
+then
+    echo "Docker daemon is not running or you have unsufficient permissions!"
+    exit 1
+fi
+
+APP_NAME="steamcmd"
+docker build --tag "$APP_NAME" .
+
+read -p "Test image? [y/n]" -n 1 -r && echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	docker run \
+	--rm \
+	--interactive \
+	"$APP_NAME" ./steamcmd.sh +login anonymous +quit
+fi
