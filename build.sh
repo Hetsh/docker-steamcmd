f1454f6f410c5e07fc8fd9ef18e2f8288f296d2c
Hetsh <git@hetsh.de>
2020-03-14 20:43:38 +0100

    Fix typos, sync time during test

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ cd "$CWD"
 # Load helpful functions
 source libs/common.sh
 
-# Check access do docker daemon
+# Check access to docker daemon
 assert_dependency "docker"
 if ! docker version &> /dev/null; then
-    echo "Docker daemon is not running or you have unsufficient permissions!"
-    exit -1
+	echo "Docker daemon is not running or you have unsufficient permissions!"
+	exit -1
 fi
 
 # Build the image
@@ -26,6 +26,7 @@ if confirm_action "Test image?"; then
 	docker run \
 	--rm \
 	--interactive \
+	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 	--name "$APP_NAME" \
 	"$APP_NAME" steamcmd.sh +login anonymous +quit
 fi
